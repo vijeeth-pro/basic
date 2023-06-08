038e098180d65c7d528618cbfa1afc067bc5aa86
vijeeth-pro <thisisvijeeth>
2023-06-08 11:15:25 +0530

    tes

--- src/App.js
+++ src/App.js
@@ -3,29 +3,46 @@ import './App.css';
 import Name from './page/Name';
 import { useEffect, useState } from 'react';
 import List from './components/List';
+import openEye from './assets/open-eye.png'
+import closeEye from './assets/close-eye.png'
 // jsx
 
 function App() {
-  const [name, updateName] = useState('vijeeth') // ['vijeeth', function]
-  const [todo, setTodo] = useState(['todo1', 'todo2', 'todo3']) // ['vijeeth', function
-  const [inputValue, setInputValue] = useState('') 
+  const [open, setOpen] = useState(false)
   
-  const handleList = () => {
-    setTodo([...todo, inputValue])
-  }
-
-  const handleDelete = (index) => {
-    // console.log(index)
-    const newTodo = todo.filter((item, i) => i !== index)
-    console.log(newTodo)
-    setTodo(newTodo)
-  }
 
   return (
     <div className='todo'>
-      {/* {name}
+      <div className='password'>
+        <input placeholder='Password'
+        type={open ? 'password': 'text'}/>
+        {open ? 
+        <img src={openEye} onClick={() => setOpen(!open)} alt='open'/> 
+        : <img src={closeEye} onClick={() => setOpen(!open)} alt='close'/>}
+      </div>
+
+    </div>  
+  );    
+}
+
+export default App;
+// const [name, updateName] = useState('vijeeth') // ['vijeeth', function]
+//   const [todo, setTodo] = useState(['todo1', 'todo2', 'todo3']) // ['vijeeth', function
+//   const [inputValue, setInputValue] = useState('') 
+  
+//   const handleList = () => {
+//     setTodo([...todo, inputValue])
+//   }
+
+//   const handleDelete = (index) => {
+//     // console.log(index)
+//     const newTodo = todo.filter((item, i) => i !== index)
+//     console.log(newTodo)
+//     setTodo(newTodo)
+//   }
+{/* {name}
       <button onClick={changeName}>Change Name</button> */}
-      <input placeholder='list' onChange={(e) => setInputValue(e.target.value)}/>
+      {/* <input placeholder='list' onChange={(e) => setInputValue(e.target.value)}/>
       <button onClick={() => handleList()}>Add Todo</button>
       <ul>
         {todo.map((item, index) => 
@@ -33,10 +50,4 @@ function App() {
         setTodo={setTodo}
         handleDelete={handleDelete} />)}  
 
-      </ul>
-
-    </div>  
-  );    
-}
-
-export default App;
+      </ul> */}
